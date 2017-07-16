346cb8baf0c24e91fabed368bc23ac1010b66217
Marcos Scriven <marcos@scriven.org>
2017-07-16 11:45:17 +0100

    Add a script for initial install during recovery.

---
+++ galeforce/bin/recovery.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+ROOT="$(dirname "$0")/../.."
+TEMP_MOUNT="/tmp/galeforce/installmnt"
+NEW_USR_LOCAL="$TEMP_MOUNT/dev_image"
+
+function makeNewUsrLocal() {
+    mkdir -p "$TEMP_MOUNT"
+    mount /dev/mmcblk0p1 "$TEMP_MOUNT"
+    # Upon restart this gets mapped to /usr/local
+    mkdir -p "$NEW_USR_LOCAL"
+}
+
+function copyGaleforce() {
+    cp -R "$ROOT/galeforce" "$NEW_USR_LOCAL"
+    chmod u+x "$NEW_USR_LOCAL/galeforce/bin/*"
+    chmod 700 "$NEW_USR_LOCAL/galeforce/bin/dropbear"
+}
+
+function linkBinaries() {
+    ln -s "$NEW_USR_LOCAL/galeforce/bin/dropbear" "/usr/local/bin"
+    ln -s "$NEW_USR_LOCAL/galeforce/bin/busybox" "/usr/local/bin/wget"
+
+    # TODO Link busybox dynamically
+
+    ln -s "$NEW_USR_LOCAL/galeforce/bin/busybox" "/usr/local/bin/vi"
+    # TODO Link busybox dynamically
+}
+
+makeNewUsrLocal
+copyGaleforce
+linkBinaries
+
